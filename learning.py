ed1a3248bd54849d8356bf025fa86e111cfd429f
Avia Efrat <avia.efrat@gmail.com>
2019-02-17 13:45:55 +0200

    Improve preprocessing
    The multi_error-mean achieved was ~0.095

--- learning.py
+++ learning.py
@@ -1,9 +1,10 @@
 import lightgbm as lgb
 
+from constants import abc, abc_lower, accents, accents_lower, basic_punctuation, other_symbols
 from data_loader import load_data
-from preprocess import (create_char_proportions_feature, create_unicode_block_proportions_feature,
+from preprocess import (create_unicode_block_proportions_feature,
                         extract_char_ngrams_feature, remove_retweets, remove_urls,
-                        extract_n_hashtags_feature)
+                        extract_n_hashtags_feature, remove_handles, reduce_lengthening)
 
 
 data = load_data()
@@ -11,11 +12,15 @@ X = data.drop(['lang'], axis=1)
 y, _ = data['lang'].factorize()
 
 X['tweet_text'] = remove_retweets(X['tweet_text'])
+X['tweet_text'] = remove_handles(X['tweet_text'])
 X['tweet_text'] = remove_urls(X['tweet_text'])
+X['tweet_text'] = reduce_lengthening(X['tweet_text'])
+
 # X['n_hashtags'] = extract_n_hashtags_feature(X['tweet_text'])
 X = create_unicode_block_proportions_feature(X)
-X = create_char_proportions_feature(X)
-X = extract_char_ngrams_feature(X, chars=[chr(i) for i in range(32, 47)] + [chr(i) for i in range(91, 126)], n=2)
+X = extract_char_ngrams_feature(X, chars=abc+accents, n=1, ignore_case=False, ratio_of_total=False)
+X = extract_char_ngrams_feature(X, chars=other_symbols, n=1, ignore_case=False, ratio_of_total=False)
+X = extract_char_ngrams_feature(X, chars=abc_lower+accents_lower+basic_punctuation, n=2, ratio_of_total=False)
 X = X.drop(['tweet_text'], axis=1)
 
 train_data = lgb.Dataset(X, label=y)
